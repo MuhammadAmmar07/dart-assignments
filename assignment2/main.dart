5da14c573cd69bf00029a971c408efc97d78a861
MuhammadAmmar07 <161923163+MuhammadAmmar07@users.noreply.github.com>
2024-03-22 22:53:48 +0500

    fourth assignment

---
+++ assignment2/main.dart
@@ -0,0 +1,91 @@
+main() {
+// ==========Q1==========
+ int breadth = 5;
+ int lenght = 5;
+
+ if(lenght == breadth){
+   print('Square');
+ }
+ else{
+    print('Rectangle');
+ }
+
+// ==========Q2==========
+
+ int age1 = 17;
+ int age2 = 16;
+
+ if(age1>age2){
+   print("Youngest");
+ }
+ else{
+   print("Smaller");
+ }
+
+// ==========Q3==========
+ num classes = 16;
+ num attendedClass = 10;
+ num percentage = (attendedClass/classes)*100;
+ print(percentage);
+
+ if(percentage>=75){
+   print("Student is allowed ");
+ }
+ else{
+   print("student is not allowed");
+ }
+
+// ==========Q4==========
+ num year = 2025;
+ num LeapYear = year % 4 ;
+ print(LeapYear);
+ if(LeapYear == 0){
+   print("it is a leap year");
+ }
+ else{
+   print("it is not a leap year");
+ }
+
+
+
+
+// ==========Q5==========
+ num temperature = 42;
+ if(temperature < 0){
+   print("Freezing weather");
+ }
+ else if(temperature == 0 && temperature <=10){
+   print("Very Cold Weather");
+ }
+ else if(temperature== 11 && temperature <=20 ){
+   print("Cold weather");
+ }
+ else if(temperature == 21 && temperature <=30){
+   print("Normal Temperature");
+ }
+ else if(temperature == 31 && temperature <=40){
+   print("Hot");
+ }
+ else if(temperature>=40){
+   print("Very hot");
+ }
+
+
+
+// ==========Q6==========
+ String alphabet = 'b';
+ if(alphabet == "a" || alphabet=="e" || alphabet=="i"|| alphabet=="o" || alphabet=="u"){
+   print("It is a vowel");
+ }
+ else{
+   print("It is a consonant");
+ } 
+
+
+
+
+
+
+
+
+}
